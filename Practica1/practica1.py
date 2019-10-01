ffe8e2ee472a20c30cf8fcb6247ef6b8eb9f29b1
Diego BV <diego.baratto8@gmail.com>
2019-10-01 23:33:15 +0200

    Actualizadas funciones a las vistas en clase

--- Practica1/practica1.py
+++ Practica1/practica1.py
@@ -16,38 +16,46 @@ def load_csv(file_name):
     return values.astype(float)
 
 def h(x, _theta):
-    return (_theta[0] + x*_theta[1])
+    return (np.dot(x, np.transpose(_theta))) #scalar product
 
-def J(_cases, _theta):
-    m = len(_cases)
-    return 1/(2*m) * np.sum((h(_cases[:, 0], _theta) - _cases[:, 1])**2)
+def J(X, Y, m, _theta):
+    return 1/(2*m) * np.sum((h(X, _theta) - Y)**2)
 
-def minimize(_cases, _theta):
+def minimize(X, Y, m, _theta):
     """
     Minimize using the given formulas
     """
-    m = len(_cases)
-    temp0 = _theta[0] - learning_rate*(1/m) * np.sum(h(_cases[:, 0], _theta) - _cases[:,1])
-    temp1 = _theta[1] - learning_rate*(1/m) * np.sum((h(_cases[:, 0], _theta) - _cases[:,1]) * _cases[:, 0])
+    H = h(X, _theta)
+    temp0 = _theta[0][0] - learning_rate*(1/m) * np.sum(H - Y)
+    temp1 = _theta[0][1] - learning_rate*(1/m) * np.sum((H - Y) *  X)
 
-    _theta[0] = temp0
-    _theta[1] = temp1
+    _theta[0, 0] = temp0
+    _theta[0, 1] = temp1
 
 
-def pinta_todo(_cases, _theta):
-    plt.figure()
-    plt.scatter(_cases[:,0], _cases[:,1], 1, "red")
-    plt.plot(_cases[:,0], h(_cases[:,0], _theta), color="grey")
+def pinta_todo(X, Y, _theta):
+    #plt.scatter(X, Y, 1, "red")
+    plt.plot(X[:, 1:], h(X, _theta), color="grey")
     plt.show()
 
-def gradient_descent_loop():
-    theta = [0,0]
-    cases = load_csv("ex1data1.csv")
-
+def gradient_descent_loop(X, Y, m):
+    theta = np.array([[0, 0]], dtype=float)
+    cost = np.array([], dtype=float)
     for i in range(M):
-        minimize(cases, theta)
-        print(J(cases, theta))
+        minimize(X, Y, m, theta)
+        np.append(cost, J(X, Y, m, theta))
     
-    pinta_todo(cases, theta)
+    return theta, cost
 
-gradient_descent_loop()
+plt.figure()
+data = load_csv("ex1data1.csv")
+X = data[:, :-1] #every col but the last
+m = np.shape(X)[0] #number of training examples
+Y = data[:, -1] #the last col, every row
+Y = np.reshape(Y, (m, 1))
+plt.scatter(X, Y, 1, "red")
+X = np.hstack([np.ones([m, 1]), X]) 
+
+theta, cost = gradient_descent_loop(X, Y, m)
+
+pinta_todo(X, Y, theta)
